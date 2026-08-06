faa2eef2ab00425bad7a984e4a9cf7c2ea6cd1a0
anasbughio <anasbughio@gmail.com>
2026-08-06 12:16:02 +0500

    showcase section completed

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
-import type { NextConfig } from "next";
+import type { Config } from "tailwindcss";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        serif: ["var(--font-playfair)", "serif"],
+      },
+      // ADD THESE ANIMATIONS
+      keyframes: {
+        slowZoom: {
+          '0%': { transform: 'scale(1)' },
+          '100%': { transform: 'scale(1.05)' },
+        }
+      },
+      animation: {
+        'slow-zoom': 'slowZoom 10s ease-out forwards',
+      }
+    },
+  },
+  plugins: [],
 };
 
-export default nextConfig;
+export default config;
